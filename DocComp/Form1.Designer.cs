9a8623461783f110ca48cd58d70010e380e7843d
Chris Niehaus <cln2482@outlook.com>
2017-07-11 16:56:12 -0500

    Initial Commit. Project goal is to create a doc parser and compare a job description with a resume.  Then give a score based on weighted keywords.

--- DocComp/Form1.Designer.cs
+++ DocComp/Form1.Designer.cs
@@ -28,12 +28,146 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btn_Compare = new System.Windows.Forms.Button();
+            this.btn_LoadJobDesc = new System.Windows.Forms.Button();
+            this.btn_LoadResume = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.lbl_ResCount = new System.Windows.Forms.Label();
+            this.lbl_JobCount = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listBox2);
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Controls.Add(this.btn_LoadJobDesc);
+            this.groupBox1.Controls.Add(this.btn_LoadResume);
+            this.groupBox1.Location = new System.Drawing.Point(199, 33);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(631, 414);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // btn_Compare
+            // 
+            this.btn_Compare.Location = new System.Drawing.Point(68, 33);
+            this.btn_Compare.Name = "btn_Compare";
+            this.btn_Compare.Size = new System.Drawing.Size(75, 23);
+            this.btn_Compare.TabIndex = 9;
+            this.btn_Compare.Text = "Compare";
+            this.btn_Compare.UseVisualStyleBackColor = true;
+            // 
+            // btn_LoadJobDesc
+            // 
+            this.btn_LoadJobDesc.Location = new System.Drawing.Point(428, 360);
+            this.btn_LoadJobDesc.Name = "btn_LoadJobDesc";
+            this.btn_LoadJobDesc.Size = new System.Drawing.Size(75, 23);
+            this.btn_LoadJobDesc.TabIndex = 8;
+            this.btn_LoadJobDesc.Text = "Load";
+            this.btn_LoadJobDesc.UseVisualStyleBackColor = true;
+            this.btn_LoadJobDesc.Click += new System.EventHandler(this.btn_LoadJobDesc_Click);
+            // 
+            // btn_LoadResume
+            // 
+            this.btn_LoadResume.Location = new System.Drawing.Point(128, 361);
+            this.btn_LoadResume.Name = "btn_LoadResume";
+            this.btn_LoadResume.Size = new System.Drawing.Size(75, 23);
+            this.btn_LoadResume.TabIndex = 7;
+            this.btn_LoadResume.Text = "Load";
+            this.btn_LoadResume.UseVisualStyleBackColor = true;
+            this.btn_LoadResume.Click += new System.EventHandler(this.btn_LoadResume_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lbl_JobCount);
+            this.groupBox2.Controls.Add(this.lbl_ResCount);
+            this.groupBox2.Controls.Add(this.btn_Compare);
+            this.groupBox2.Location = new System.Drawing.Point(837, 33);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 414);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(86, 19);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(166, 316);
+            this.listBox1.TabIndex = 9;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(374, 19);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(166, 316);
+            this.listBox2.TabIndex = 10;
+            // 
+            // lbl_ResCount
+            // 
+            this.lbl_ResCount.AutoSize = true;
+            this.lbl_ResCount.Location = new System.Drawing.Point(31, 75);
+            this.lbl_ResCount.Name = "lbl_ResCount";
+            this.lbl_ResCount.Size = new System.Drawing.Size(49, 13);
+            this.lbl_ResCount.TabIndex = 10;
+            this.lbl_ResCount.Text = "resCount";
+            // 
+            // lbl_JobCount
+            // 
+            this.lbl_JobCount.AutoSize = true;
+            this.lbl_JobCount.Location = new System.Drawing.Point(31, 102);
+            this.lbl_JobCount.Name = "lbl_JobCount";
+            this.lbl_JobCount.Size = new System.Drawing.Size(49, 13);
+            this.lbl_JobCount.TabIndex = 11;
+            this.lbl_JobCount.Text = "jobCount";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1160, 535);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btn_LoadJobDesc;
+        private System.Windows.Forms.Button btn_LoadResume;
+        private System.Windows.Forms.Button btn_Compare;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label lbl_JobCount;
+        private System.Windows.Forms.Label lbl_ResCount;
     }
 }
 
--- DocComp/Form1.cs
+++ DocComp/Form1.cs
@@ -1,20 +1,106 @@
-﻿using System;
+﻿using Microsoft.Office.Interop.Word;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+
 
 namespace DocComp
 {
     public partial class Form1 : Form
     {
+        string[] resume;
+        string[] jobDescription;
+        string[] keywords;
+
+
         public Form1()
         {
             InitializeComponent();
+            
         }
+
+        private void btn_LoadResume_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog()
+            {
+                InitialDirectory = "e:\\",
+                Filter = "Text Files|*.doc;*.docx;*.txt;*.text",
+                FilterIndex = 2,
+                RestoreDirectory = true
+            };
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                Microsoft.Office.Interop.Word.Application app = new Microsoft.Office.Interop.Word.Application();
+
+                char[] delim = { '\r','|',' ', ',', '.', ':', ';', '-' };
+                string filename = ofd.FileName;
+                
+                Document doc = app.Documents.Open(filename);
+
+                string[] parsed = doc.Content.Text.Split(delim).ToArray();
+
+                populateResumeBox(parsed);
+                app.Quit();
+                ofd.Dispose();
+            }
+        }
+
+        private void btn_LoadJobDesc_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog()
+
+            {
+                InitialDirectory = "e:\\",
+                Filter = "Text Files|*.doc;*.docx;*.txt;*.text",
+                FilterIndex = 2,
+                RestoreDirectory = true
+            };
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                Microsoft.Office.Interop.Word.Application app = new Microsoft.Office.Interop.Word.Application();
+
+                char[] delim = { ' ', ',', '.', ':', ';', '-' };
+                string filename = ofd.FileName;
+
+                Document doc = app.Documents.Open(filename);
+
+                string[] parsed = doc.Content.Text.Split(delim).ToArray();
+
+                populateJobDescriptionBox(parsed);
+                app.Quit();
+                ofd.Dispose();
+            }
+        }
+
+        private void populateResumeBox(string[] list)
+        {
+            lbl_ResCount.Text = list.Count().ToString();
+
+            foreach (string s in list)
+            {
+                listBox1.Items.Add(s);
+            }
+        }
+
+        private void populateJobDescriptionBox(string[] list)
+        {
+            lbl_JobCount.Text = list.Count().ToString();
+
+            foreach (string s in list)
+            {
+                listBox2.Items.Add(s);
+            }
+        }
+
+        private void compare(string[] list, string[] list2)
+        {
+
+        }
+
     }
 }
+
